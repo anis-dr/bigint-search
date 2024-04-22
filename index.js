1981e5393d2bef060f92abb410ffbc9b03a48703
Anis Lazaar <anis00723@gmail.com>
2024-04-22 20:36:23 +0100

    remove node_modules

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // Database connection details
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'your_username',
-  password: 'your_password',
-  database: 'tt-api',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 connection.connect((err) => {
